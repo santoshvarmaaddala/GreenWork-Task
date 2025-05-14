345e97d94e67d097498539f7d99860dfbedbb794
santoshvarmaaddala <santoshvarma2166@gmail.com>
2025-05-14 10:00:45 +0530

    Postgres Connected: postgres connected with backend

--- employee_project/settings.py
+++ employee_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,13 +78,9 @@ WSGI_APPLICATION = 'employee_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
-
-
+print(dj_database_url.config(default=config('DATABASE_URL')))
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 

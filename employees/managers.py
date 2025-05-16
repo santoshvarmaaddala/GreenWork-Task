f3113f7192ee3c8073241ded32c839f0a8041f18
santoshvarmaaddala <santoshvarma2166@gmail.com>
2025-05-16 23:28:14 +0530

    Unit Test Added

--- employees/managers.py
+++ employees/managers.py
@@ -1,8 +1,36 @@
 from django.contrib.auth.models import UserManager
+from django.core.exceptions import ValidationError
+
 
 class EmployeeManager(UserManager):
-    def create_superuser(self, email, password=None, **extra_fields):
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Create and save a user with the given email and password.
+        """
         if not email:
-            raise ValueError('The Email must be set')
+            raise ValueError("The Email must be set")
         email = self.normalize_email(email)
-        return super().create_superuser(username=email, email=email, password=password, **extra_fields)
+
+        # Use email as username (AbstractUser still requires a username internally)
+        extra_fields.setdefault('username', email)  # <-- Add this line
+
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
